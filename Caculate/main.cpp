b4e2e42c14637c16c06ffaa90154f2c3e2964c25
cugxy <cugxy@qq.com>
2017-11-20 17:18:29 +0800

    finish

--- Caculate/main.cpp
+++ Caculate/main.cpp
@@ -1,7 +1,8 @@
+#include "libInclude.h"
 #include <osg/Vec3>
 #include <vector>
 
-std::vector<osg::Vec3f> Calculate(osg::Vec3f v1, osg::Vec3f v2, float fWidth)
+std::vector<osg::Vec3f> Calculate(osg::Vec3f &v1, osg::Vec3f &v2, float &fWidth)
 {
 	std::vector<osg::Vec3f> result;
 	if (fWidth <= 0.0)
@@ -16,7 +17,7 @@ std::vector<osg::Vec3f> Calculate(osg::Vec3f v1, osg::Vec3f v2, float fWidth)
 	float fx = -kV1V2.y();
 	float fy = kV1V2.x();
 	osg::Vec3f tmpV(fx, fy, 0.0); //垂直单位向量
-	osg::Vec3f kV = tmpV * (fWidth / 2); 
+	osg::Vec3f kV =  tmpV * (fWidth / 2);
 
 	//注意顺序
 	osg::Vec3f rV1 = v1 + kV;
@@ -35,7 +36,5 @@ int main()
 	osg::Vec3f vt1(0.0, 0.0, 0.0);
 	osg::Vec3f vt2(1.732, 1.0, 0.0);
 	float ft1 = 4;
-
 	auto result = Calculate(vt1, vt2, ft1);
-	return 0;
 }
