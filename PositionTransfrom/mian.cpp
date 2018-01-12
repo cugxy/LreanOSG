d22eeb9720414603551c29c96f48c5ff79679935
cugxy <cugxy@qq.com>
2018-01-12 13:50:35 +0800

    unkown error

--- PositionTransfrom/mian.cpp
+++ PositionTransfrom/mian.cpp
@@ -21,6 +21,7 @@ int main()
 
 	ref_ptr<Group> root = new Group();
 	ref_ptr<Node> node = readNodeFile(kstrDataPath + "cow.osg");
+
 	ref_ptr<PositionAttitudeTransform> pat1 = new PositionAttitudeTransform();
 	pat1->setPosition(Vec3(-10.0f, 0.0f, 0.0f));
 	pat1->setScale(Vec3(0.5f, 0.5f, 0.5f));
@@ -30,16 +31,24 @@ int main()
 	pat2->setPosition(Vec3(10.0f, 0.0f, 0.0f));
 	pat2->addChild(node.get());
 
-	ref_ptr<MatrixTransform> mt = new MatrixTransform();
+	ref_ptr<MatrixTransform> mt1 = new MatrixTransform();
 	Matrix m;
 	m.makeTranslate(Vec3(10.0f, 0.0f, 0.0f));
 	m.makeRotate(45.0f, 1.0f, 0.0f, 0.0f);
-	mt->setMatrix(m);
-	mt->addChild(node.get());
+	mt1->setMatrix(m);
+	mt1->addChild(node.get());
+
+	ref_ptr<MatrixTransform> mt2 = new MatrixTransform();
+	Matrix t;
+	t.makeTranslate(Vec3(-10.0f, 0.0f, 0.0f));
+	mt2->setMatrix(t);
+	mt2->addChild(node.get());
+
 
 	root->addChild(pat1.get());
 	root->addChild(pat2.get());
-	root->addChild(mt.get());
+	root->addChild(mt1.get());
+	root->addChild(mt2.get());
 
 	osgUtil::Optimizer opt;
 	opt.optimize(root.get());
